3fe6738433fde3f64c1440fd600135e33da796b6
Recep  Eren <eren@C02YT245LVCF-652BA.local>
2021-07-26 11:56:01 -0400

    gam New user add

--- gam.sh
+++ gam.sh
@@ -6,3 +6,12 @@ gam csv /Users/eren/Desktop/passwords.csv gam update user ~email password ~Pass
 
 # Do not allow to change password for users (/Users/eren/Desktop/passwords.csv is csv file location)
 gam csv /Users/eren/Desktop/passwords.csv gam update user ~email changepassword off
+
+
+# Create new Users
+
+gam create user <email address> firstname <First Name>
+ lastname <Last Name> password <Password>
+ [suspended on|off] [changepassword on|off]
+ [gal on|off] [sha] [md5] [crypt] [nohash]
+ [org <Org Name>] [recoveryemail <email> [recoveryphone <phone>]
