4242d63f78f31a064bc13275fd2feec4d6a9fab6
Recep  Eren <eren@C02YT245LVCF-652BA.local>
2021-09-17 11:20:18 -0400

    TRY

---
+++ gamMailPass2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import csv
+
+with open ("/Users/eren/SchoolScripts/PassMail.csv", 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)
+    for line in csv_reader:
+        # print(f'gam update user {line[1]} password {line[2]}')
+        # print(f'gam update user {line[0]} password {line[0][0:3]}'+'12345')
+        # print(f'{line[0]}')
+        # print(f'{line[0][0:3]}'+'12345')
+        print(f'{line[0]} -- {line[0][0:3]}'+'12345')  # For Teacher
+
+sdsd sdsdsd
+
+
+
+# gam update user c1 password password12
