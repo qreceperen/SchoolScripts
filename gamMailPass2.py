4a6753ee6e4ec648b0cbff252cfecfaf99b11c78
Recep  Eren <eren@C02YT245LVCF-652BA.local>
2021-10-07 09:13:43 -0400

    csv file is readed as dictionary

--- gamMailPass2.py
+++ gamMailPass2.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-
 import csv
 
 with open ("/Users/eren/SchoolScripts/PassMail.csv", 'r') as csv_file:
-
-    csv_reader = csv.reader(csv_file)
+    csv_reader = csv.DictReader(csv_file)
     next(csv_reader)
+
     for line in csv_reader:
-        # print(f'gam update user {line[1]} password {line[2]}')
+        # print(f'gam update user {line["e-mail"]} password {line["Pass"]}') # Update user mail and password
+        # print(f"gam update group {line['group']} add member {line['student1']") # Update user group
         # print(f'gam update user {line[0]} password {line[0][0:3]}'+'12345')
         # print(f'{line[0]}')
         # print(f'{line[0][0:3]}'+'12345')
-        print(f'{line[0]} -- {line[0][0:3]}'+'12345')  # For Teacher
+        # print(f'{line[0]} -- {line[0][0:3]}'+'12345')  # For Teacher
 
 
 
-gam update group samplegroup add member student1@bascs.org
 
+# gam update group samplegroup add member student1@bascs.org
 # gam update user c1 password password12
