312a66be6fb843e7924174a52f9b5f20b556a93b
Recep  Eren <eren@C02YT245LVCF-652BA.local>
2022-01-21 12:41:06 -0500

    script is ready to find folder inside the folder

--- removePastCameraRecords.py
+++ removePastCameraRecords.py
@@ -9,8 +9,7 @@ file_path = "/Users/eren/Desktop/Dates 2"
 
 for root, subdirs, files in os.walk(file_path):
     for subdir in subdirs:
-        if subdir[0:2] == "*":
-        # if subdir[0:2] == "*" and subdir[6:10] == "2022":
+        if subdir[0:2] == "12" and subdir[6:10] == "2021":
             print(subdir)
 
     # print ("current path", dirpath)
