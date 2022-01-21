7d2148ab49efdd6b134fcaa6c6b2f97152a53a73
Recep  Eren <eren@C02YT245LVCF-652BA.local>
2022-01-21 13:17:38 -0500

    Find how to write full path for the folder you are looking for

--- removePastCameraRecords.py
+++ removePastCameraRecords.py
@@ -3,20 +3,20 @@
 import os
 from posixpath import dirname
 from re import sub
+import shutil
 
+
+
+#? How copy user name from computer and automatically combine with path. We can have windows machine or mac or linux. Is there a way to take and combine all.
 file_path = "/Users/eren/Desktop/Dates 2"
-
-
 for root, subdirs, files in os.walk(file_path):
     for subdir in subdirs:
-        if subdir[0:2] == "12" and subdir[6:10] == "2021":
-            print(subdir)
-
-    # print ("current path", dirpath)
-    # print ("Directories", dirnames)
-    # print ("Files", filenames)
+        print(subdir)
+        # if subdir[0:2] == "12" and subdir[6:10] == "2021": #take to date change the date numbers according to folder name.
 
 
-# print(os.listdir(file_path))
+            # deletePath = file_path+subdir
+            # print(deletePath)
 
-asdasdsad
+
+
