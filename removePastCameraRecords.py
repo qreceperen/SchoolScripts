da7fb583d0841af5cc07d093f47152fff991d686
receperen <qreceperen@gmail.com>
2022-01-23 12:39:08 -0500

    I need to solve iteration Problem. Need to find each folder creation time instead of finding main folder creation time according to number of subfolders

--- removePastCameraRecords.py
+++ removePastCameraRecords.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python3
 
+from dataclasses import field
 import os
 from re import sub
 import shutil
+import time
 
 
 #? How copy user name from computer and automatically combine with path. We can have windows machine or mac or linux. Is there a way to take and combine all.
-file_path = "/Users/eren/Desktop/Original 2" # Write main directory of camera records.
+file_path = r'''C:\Users\recep\Desktop\1''' # Write main directory of camera records. (For Windows)
+# file_path = "C:\Users\recep\Desktop\1" # Write main directory of camera records.
 for root, subdirs, files in os.walk(file_path):
     for subdir in subdirs:
-        if subdir[0:2]=="12" and subdir[6:10]=="2021": # first number is month such as 01,02,03,04 second number is year. The date format in the camera folder is 01.17.2021. You can change month and year number to delete previous month records.
-            fileDirectory = os.path.abspath(os.path.join(root,subdir)) #it finds the original directory path
-            print(fileDirectory)
-            shutil.rmtree(fileDirectory)
+        file_path = file_path +"\\" +subdir
+        fileCreationTime = os.stat(file_path).st_ctime
+        print(fileCreationTime)
+        
+        # Create subdir creation time
+       
+        # if subdir[0:2]=='12':
+        #     # File creation time is problematic. It iterates main folder creation time. Thats why all the time are same.
+        #     # Need to find a way to iterate files inside the main folder.
+        #     file_path = file_path +"\\" +subdir
+        #     fileCreationTime = os.stat(file_path).st_ctime
+        #     print(fileCreationTime)
+        #     systemCurrentTime = time.time()
+        #     systemTime_FileCreationTime_Difference = int(systemCurrentTime - fileCreationTime) # it removes decimal points.
+        #     #NOTE 60 Unix Time/1 Min
+        #     fileDirectory = os.path.abspath(os.path.join(root,subdir)) #it finds the original directory path
+        #     # print(fileDirectory , fileCreationTime, systemCurrentTime, round(systemTime_FileCreationTime_Difference/3600,2))
+        
+        #     # print(fileDirectory , fileCreationTime, systemCurrentTime, systemTimeFileCreationTimeDifference)
+        # # Create system current time
+        # # Compare subdir creation time and system creation time
+        
+    
+
+
+        # shutil.rmtree(fileDirectory)
